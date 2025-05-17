b87e3ddfac690ba0325d6091df72368eedb39cb8
zyk <2126483838@qq.com>
2025-05-17 15:55:20 +0800

    linux搜索文件C函数

---
+++ py/find_file.c
@@ -0,0 +1,38 @@
+/**
+ * filename:find_file.c
+ * description: 搜索linux0.11下的文件
+ * author:EthanYankang
+ * create time:2025/05/17 15:41:46
+ */
+#include <stdio.h>
+#include <string.h>
+#define PATH "/home/eyk/1code/Linux-0.11code/py/line.txt"
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("usage: find_file <filename>\nPlease enter the filename you want to search for(only one name).\n");
+    }
+    /**
+     * data descp: then argv[1] is the filename we want to search for
+     */
+    char *filename = argv[1];
+    /**
+     * data descp: the root directory of linux0.11
+     */
+    FILE *fp = fopen(PATH, "r");
+    if (fp == NULL)
+    {
+        printf("Failed to open file.\n");
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), fp))
+    {
+        if (strstr(line, filename) != NULL)
+        {
+            printf("%s", line);
+        }
+    }
+    fclose(fp);
+    return 0;
+}
